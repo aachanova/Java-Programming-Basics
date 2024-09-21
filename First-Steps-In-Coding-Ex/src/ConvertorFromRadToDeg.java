088d69bd3af9dca3a7da629d5d821969cd43b0b3
Ani <anelia_9@yahoo.com>
2024-09-22 00:46:12 +0300

    created ConvertorFromRadToDeg

--- First-Steps-In-Coding-Ex/src/ConvertorFromRadToDeg.java
+++ First-Steps-In-Coding-Ex/src/ConvertorFromRadToDeg.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ConvertorFromRadToDeg {
     public static void main(String[] args) {
+        // degree = rad * 180 / π
         Scanner scanner = new Scanner(System.in);
         double radians = Double.parseDouble(scanner.nextLine());
         double degrees = radians * (180 / Math.PI);
