17664c454613fffdaf556f1842adc9752791650d
Ani <anelia_9@yahoo.com>
2024-09-22 01:05:03 +0300

    created VacationLiterature

--- First-Steps-In-Coding-Ex/src/VacationLiterature.java
+++ First-Steps-In-Coding-Ex/src/VacationLiterature.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class VacationLiterature {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int pagesCount = Integer.parseInt(scanner.nextLine());
         int pagesPerHour = Integer.parseInt(scanner.nextLine());
         int days = Integer.parseInt(scanner.nextLine());
