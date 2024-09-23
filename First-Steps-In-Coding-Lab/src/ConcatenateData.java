cdd6ea52f5d10e2ecf4dcd9bf57b0802a09bcb91
Ani <anelia_9@yahoo.com>
2024-09-23 23:19:44 +0300

    created ConcatenateData

---
+++ First-Steps-In-Coding-Lab/src/ConcatenateData.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConcatenateData {
+    public static void main(String[] args) {
+        //име, фамилия, възраст и град и печата следното съобщение: "You are <firstName> <lastName>, a <age>-years old person from <town>."
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String town = scanner.nextLine();
+
+        System.out.println("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".");
+    }
+}
