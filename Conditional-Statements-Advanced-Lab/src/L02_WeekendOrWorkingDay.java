2a5c0adb38dbf3bfd05815e109e371fb449ee7f3
Ani <anelia_9@yahoo.com>
2024-10-08 00:25:44 +0300

    created L02_WeekendOrWorkingDay

---
+++ Conditional-Statements-Advanced-Lab/src/L02_WeekendOrWorkingDay.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class L02_WeekendOrWorkingDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOfWeek = scanner.nextLine();
+
+        switch (dayOfWeek) {
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("Working day");
+            case "Saturday", "Sunday" -> System.out.println("Weekend");
+            default -> System.out.println("Error");
+        }
+    }
+}
