5863ef11e86354989737d52516a9fdf540d147a4
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2024-12-24 21:45:29 +0200

    added L05_CharacterSequence

---
+++ For-Loop-Lab/src/L05_CharacterSequence.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class L05_CharacterSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(text.charAt(i));
+        }
+    }
+}
